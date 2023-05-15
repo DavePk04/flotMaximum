1ec18c41c8bacc29d3d6b1e272fb5f7889658bb0
dpikoppo <dave.pikop.pokam@ulb.be>
2023-05-15 16:13:22 +0200

    refactor: made chemin_augmentant.py & generate_model.py contents as class

--- algo3/chemin_augmentant.py
+++ algo3/chemin_augmentant.py
@@ -1,64 +1,69 @@
 import sys
 
-# BFS algorithm to find a path from source to sink
-def bfs(graph, s, t, parent):
-    visited = [False] * len(graph) # mark all nodes as unvisited
-    queue = [s] # start from the source node
-    visited[s] = True # mark the source node as visited
-    while queue:
-        u = queue.pop(0) # dequeue a node
-        for ind, val in enumerate(graph[u]):
-            # if a node is not visited and there is a positive capacity edge
-            if not visited[ind] and val > 0:
-                queue.append(ind) # enqueue the node
-                visited[ind] = True # mark the node as visited
-                parent[ind] = u # update the parent of the node
-    return visited[t] # return True if there is a path from source to sink
+class MaxFlowSolver:
+    def __init__(self, filename):
+        self.filename = filename
+        self.graph = []
+        self.n = 0
+        self.s = 0
+        self.t = 0
 
-# Ford-Fulkerson algorithm to find the maximum flow
-def ford_fulkerson(graph, source, sink):
-    parent = [-1] * len(graph) # initialize the parent list with -1
-    max_flow = 0 # initialize the maximum flow as 0
-    while bfs(graph, source, sink, parent):
-        path_flow = float("Inf") # initialize the path flow as infinity
-        s = sink
-        # find the minimum residual capacity along the path from sink to source
-        while s != source:
-            path_flow = min(path_flow, graph[parent[s]][s])
-            s = parent[s]
-        max_flow += path_flow # add the path flow to the maximum flow
-        v = sink
-        # update the residual capacities of the edges and their reverse edges along the path
-        while v != source:
-            u = parent[v]
-            graph[u][v] -= path_flow
-            graph[v][u] += path_flow
-            v = parent[v]
-    return max_flow # return the maximum flow
+    def solve(self):
+        self.parse_input_file()
+        max_flow = self.ford_fulkerson()
+        print("maximal flow: {}".format(max_flow))
 
-filename = ""
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    print("Argument manquant")
+    def parse_input_file(self):
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+            self.n = int(lines[0].split()[1])  # number of nodes
+            self.s = int(lines[1].split()[1])  # source node
+            self.t = int(lines[2].split()[1])  # sink node
+            num_arcs = int(lines[3].split()[1])  # number of edges
+            self.graph = [[0] * self.n for _ in range(self.n)]  # initialize the adjacency matrix with zeros
+            for i in range(4, 4 + num_arcs):
+                u, v, capacity = map(int, lines[i].split())
+                self.graph[u][v] = capacity  # set the capacity of the edge
 
-# parse the input file
-def parse_input_file(filename):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        n = int(lines[0].split()[1]) # number of nodes
-        s = int(lines[1].split()[1]) # source node
-        t = int(lines[2].split()[1]) # sink node
-        num_arcs = int(lines[3].split()[1]) # number of edges
-        graph = [[0] * n for i in range(n)] # initialize the adjacency matrix with zeros
-        for i in range(4, 4 + num_arcs):
-            u, v, capacity = map(int, lines[i].split())
-            graph[u][v] = capacity # set the capacity of the edge
-        return n, s, t, graph
+    def bfs(self, parent):
+        visited = [False] * len(self.graph)  # mark all nodes as unvisited
+        queue = [self.s]  # start from the source node
+        visited[self.s] = True  # mark the source node as visited
+        while queue:
+            u = queue.pop(0)  # dequeue a node
+            for ind, val in enumerate(self.graph[u]):
+                # if a node is not visited and there is a positive capacity edge
+                if not visited[ind] and val > 0:
+                    queue.append(ind)  # enqueue the node
+                    visited[ind] = True  # mark the node as visited
+                    parent[ind] = u  # update the parent of the node
+        return visited[self.t]  # return True if there is a path from source to sink
 
-# call the parse_input_file function and the ford_fulkerson function to find the maximum flow
-n, s, t, graph = parse_input_file(filename)
-max_flow = ford_fulkerson(graph, s, t)
+    def ford_fulkerson(self):
+        parent = [-1] * len(self.graph)  # initialize the parent list with -1
+        max_flow = 0  # initialize the maximum flow as 0
+        while self.bfs(parent):
+            path_flow = float("Inf")  # initialize the path flow as infinity
+            s = self.t
+            # find the minimum residual capacity along the path from sink to source
+            while s != self.s:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+            max_flow += path_flow  # add the path flow to the maximum flow
+            v = self.t
+            # update the residual capacities of the edges and their reverse edges along the path
+            while v != self.s:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+        return max_flow  # return the maximum flow
 
-# print the maximum flow
-print("Le flot maximal est : ", max_flow)
+
+if __name__ == '__main__':
+    try:
+        solver = MaxFlowSolver(sys.argv[1])
+        solver.solve()
+    except IndexError:
+        print("Usage: python chemin_augmentant.py <filename>")
+        sys.exit(1)
--- algo3/generate_model.py
+++ algo3/generate_model.py
@@ -1,98 +1,91 @@
 import sys
 
-filename = ""
-if len(sys.argv) > 1:
-    filename = sys.argv[1]
-else:
-    print("Argument manquant")
+class MaxFlowSolver:
+    def __init__(self, filename):
+        self.filename = filename
+        self.n = 0
+        self.s = 0
+        self.t = 0
+        self.graph = []
 
-# parse the input file
-def parse_input_file(filename):
-    with open(filename, 'r') as f:
-        lines = f.readlines()
-        n = int(lines[0].split()[1]) # number of nodes
-        s = int(lines[1].split()[1]) # source node
-        t = int(lines[2].split()[1]) # sink node
-        num_arcs = int(lines[3].split()[1]) # number of edges
-        graph = [[0] * n for i in range(n)] # initialize the adjacency matrix with zeros
-        for i in range(4, 4 + num_arcs):
-            u, v, capacity = map(int, lines[i].split())
-            graph[u][v] = capacity # set the capacity of the edge
-        return n, s, t, graph
+    def solve(self):
+        self.parse_input_file()
+        self.remove_loops()
+        self.write_model()
 
+    def parse_input_file(self):
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+            self.n = int(lines[0].split()[1]) # number of nodes
+            self.s = int(lines[1].split()[1]) # source node
+            self.t = int(lines[2].split()[1]) # sink node
+            num_arcs = int(lines[3].split()[1]) # number of edges
+            self.graph = [[0] * self.n for _ in range(self.n)] # initialize the adjacency matrix with zeros
+            for i in range(4, 4 + num_arcs):
+                u, v, capacity = map(int, lines[i].split())
+                self.graph[u][v] = capacity # set the capacity of the edge
 
-def remove_loops(graph):
-    n = len(graph)
-    for i in range(n):
-        if graph[i][i] > 0:
-            graph[i][i] = 0
-    return graph
-
-
-
-# call the parse_input_file function and the ford_fulkerson function to find the maximum flow
-n, s, t, graph = parse_input_file(filename)
-
-
-# function to solve the maximum flow problem using glpk solve
-# this program is wrote in model-n-p.lp file
-# objective function: maximize the flow from the source to the sink (it's the sum of the outgoing edges from the source)
-# constraints: for each node i except the source and the sink, , sum of incoming edges - sum of outgoing edges = 0
-#              for the source node, sum of outgoing edges >= 0
-#              for the sink node, sum of incoming edges <= 0
-# Bounds: for each edge (i, j), the flow on the edge >= 0 and <= capacity of the edge
-def write_model(n, s, t, graph):
-    with open("model-n-p.lp", 'w') as f:
-        f.write("Maximize\n")
-        f.write(" obj: ")
-        outgoing_edges = [j for j in range(n) if graph[s][j] > 0]
-        for j in outgoing_edges:
-            f.write(" + x{}_{}".format(s, j))
-        f.write("\n\n")
-
-        f.write("Subject To\n")
+    def remove_loops(self):
+        n = len(self.graph)
         for i in range(n):
-            if i != s and i != t:
-                f.write(" c{}: ".format(i))
-                incoming_edges = [j for j in range(n) if graph[j][i] > 0]
-                outgoing_edges = [j for j in range(n) if graph[i][j] > 0]
-                for j in incoming_edges:
-                    f.write(" + x{}_{}".format(j, i))
-                for j in outgoing_edges:
-                    f.write(" - x{}_{}".format(i, j))
-                f.write(" = 0\n")
+            if self.graph[i][i] > 0:
+                self.graph[i][i] = 0
 
-        f.write(" c{}: ".format(s))
-        for j in outgoing_edges:
-            f.write(" + x{}_{}".format(s, j))
-        f.write(" = 0\n")
+    def write_model(self):
+        with open("model-n-p.lp", 'w') as f:
+            f.write("Maximize\n")
+            f.write(" obj: ")
+            outgoing_edges = [j for j in range(self.n) if self.graph[self.s][j] > 0]
+            for j in outgoing_edges:
+                f.write(" + x{}_{}".format(self.s, j))
+            f.write("\n\n")
 
-        f.write(" c{}: ".format(t))
-        incoming_edges = [j for j in range(n) if graph[j][t] > 0]
-        for j in incoming_edges:
-            f.write(" + x{}_{}".format(j, t))
-        f.write(" = 0\n\n")
+            f.write("Subject To\n")
+            for i in range(self.n):
+                if i != self.s and i != self.t:
+                    f.write(" c{}: ".format(i))
+                    incoming_edges = [j for j in range(self.n) if self.graph[j][i] > 0]
+                    outgoing_edges = [j for j in range(self.n) if self.graph[i][j] > 0]
+                    for j in incoming_edges:
+                        f.write(" + x{}_{}".format(j, i))
+                    for j in outgoing_edges:
+                        f.write(" - x{}_{}".format(i, j))
+                    f.write(" = 0\n")
 
-        f.write("Bounds\n")
-        for i in range(n):
-            for j in range(n):
-                if graph[i][j] > 0:
-                    f.write(" 0 <= x{}_{} <= {}\n".format(i, j, graph[i][j]))
+            f.write(" c{}: ".format(self.s))
+            for j in outgoing_edges:
+                f.write(" + x{}_{}".format(self.s, j))
+            f.write(" = 0\n")
 
-        f.write("\n\n")
+            f.write(" c{}: ".format(self.t))
+            incoming_edges = [j for j in range(self.n) if self.graph[j][self.t] > 0]
+            for j in incoming_edges:
+                f.write(" + x{}_{}".format(j, self.t))
+            f.write(" = 0\n\n")
 
-        f.write("Binary\n")
-        for i in range(n):
-            for j in range(n):
-                if graph[i][j] > 0:
-                    f.write(" x{}_{}\n".format(i, j))
+            f.write("Bounds\n")
+            for i in range(self.n):
+                for j in range(self.n):
+                    if self.graph[i][j] > 0:
+                        f.write(" 0 <= x{}_{} <= {}\n".format(i, j, self.graph[i][j]))
 
-        f.write("End\n")
+            f.write("\n\n")
+
+            f.write("Binary\n")
+            for i in range(self.n):
+                for j in range(self.n):
+                    if self.graph[i][j] > 0:
+                        f.write(" x{}_{}\n".format(i, j))
+
+            f.write("\n\nEnd\n")
 
 
-# remove loops from the graph
-graph = remove_loops(graph)
-# call the write_model function
-write_model(n, s, t, graph)
-
+if __name__ == "__main__":
+    try:
+        filename = sys.argv[1]
+        solver = MaxFlowSolver(filename)
+        solver.solve()
+    except IndexError:
+        print("Usage: python3 generate_model.py <filename>")
+        sys.exit(1)
 
