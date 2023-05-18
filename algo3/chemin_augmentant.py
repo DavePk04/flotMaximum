dfc01c418ed19e4b2d9cf8955eac8417d2138328
dpikoppo <dave.pikop.pokam@ulb.be>
2023-05-18 16:49:12 +0200

    fixed errors in chemin_augmentant.py

--- algo3/chemin_augmentant.py
+++ algo3/chemin_augmentant.py
@@ -1,128 +1,118 @@
-import argparse
-import math
-from collections import deque, defaultdict
+import os
+import sys
 import time
 
 
-class FordFulkerson:
-    """
-    Implementation of the Ford-Fulkerson algorithm to compute the maximum flow in a network.
-    """
+class MaxFlowSolver:
     def __init__(self, filename):
-        self._filename = filename
-        self._graph = None
-        self._n = 0
-        self._s = 0
-        self._t = 0
-        self._max_flow = 0
-        self._parse_input_file()
-        self._remove_loops()
-        self._remove_incoming_arcs_to_s()
-        self._remove_outgoing_arcs_from_t()
-        self._compute_max_flow()
+        self.filename = filename
+        self.graph = []
+        self.n = 0
+        self.s = 0
+        self.t = 0
 
+    def solve(self):
+        self.parse_input_file()
+        self.remove_cycles()
+        max_flow = self.ford_fulkerson()
+        print("maximal flow: {}".format(max_flow))
+        return max_flow
 
-    def _parse_input_file(self):
-        with open(self._filename, 'r') as f:
-            self._n = int(f.readline().split()[1])
-            self._s = int(f.readline().split()[1])
-            self._t = int(f.readline().split()[1])
-            num_arcs = int(f.readline().split()[1])
-            self._graph = defaultdict(dict)
-            for _ in range(num_arcs):
-                u, v, capacity = map(int, f.readline().split())
-                self._graph.setdefault(u, {})[v] = capacity
-                self._graph.setdefault(v, {})[u] = 0
+    def parse_input_file(self):
+        with open(self.filename, 'r') as f:
+            lines = f.readlines()
+            self.n = int(lines[0].split()[1])  # number of nodes
+            self.s = int(lines[1].split()[1])  # source node
+            self.t = int(lines[2].split()[1])  # sink node
+            num_arcs = int(lines[3].split()[1])  # number of edges
+            self.graph = [[0] * self.n for _ in range(self.n)]  # initialize the adjacency matrix with zeros
+            for line in lines[4:4 + num_arcs]:
+                u, v, capacity = map(int, line.split())
+                self.graph[u][v] = capacity  # set edge capacity
 
-    def _bfs(self, parent):
-        queue = deque([self._s])
-        visited = set(queue)
+    def remove_cycles(self):
+        for i in range(self.n):
+            self.graph[i][i] = 0 # remove self-loops
+            for j in range(i + 1, self.n):
+                if self.graph[i][j] > 0 and self.graph[j][i] > 0:
+                    # If there is an edge from i to j and an edge from j to i, remove the edge from j to i
+                    self.graph[j][i] = 0
+
+    def bfs(self, parent):
+        visited = [False] * len(self.graph)  # mark all nodes as unvisited
+        queue = [self.s]  # start from the source node
+        visited[self.s] = True  # mark the source node as visited
         while queue:
-            u = queue.popleft()
-            for v, capacity in self._graph[u].items():
-                if capacity > 0 and v not in visited:
-                    queue.append(v)
-                    visited.add(v)
-                    parent[v] = u
-                    if v == self._t:
-                        return True
-        return False
+            u = queue.pop(0)  # dequeue a node
+            for ind, val in enumerate(self.graph[u]):
+                # if a node is not visited and there is a positive capacity edge
+                if not visited[ind] and val > 0:
+                    queue.append(ind)  # enqueue the node
+                    visited[ind] = True  # mark the node as visited
+                    parent[ind] = u  # update the parent of the node
+        return visited[self.t]  # return True if there is a path from source to sink
 
-    def _compute_max_flow(self, time_limit=300):
-        parent = [-1] * self._n
-        self._max_flow = 0
-        start_time = time.time()
-        while self._bfs(parent):
-            path_flow = math.inf
-            v, u = self._t, parent[self._t]
-            while v != self._s:
-                path_flow = min(path_flow, self._graph[u][v])
-                v, u = u, parent[u]
-            self._max_flow += path_flow
-            v, u = self._t, parent[self._t]
-            while v != self._s:
-                self._graph[u][v] -= path_flow
-                self._graph[v][u] += path_flow
-                v, u = u, parent[u]
-
-            elapsed_time = time.time() - start_time
-            if elapsed_time > time_limit:
-                break
-
-    def _remove_loops(self):
-        for i in range(self._n):
-            self._graph[i][i] = 0
-
-    def _remove_incoming_arcs_to_s(self):
-        for i in range(self._n):
-            self._graph[i][self._s] = 0
-
-    def _remove_outgoing_arcs_from_t(self):
-        for i in range(self._n):
-            self._graph[self._t][i] = 0
-
-    def get_max_flow(self):
-        return self._max_flow
-
-    def save_result(self, filename=None):
-        if filename is None:
-            filename = f"model-{self._filename.replace('Instances/inst-', '').replace('.txt', '')}.path"
-        with open(filename, 'w') as f:
-            f.write(f"Le flot maximal est : {self._max_flow}")
-
-    def calculate_max_flow_st_cut(self):
-        visited = set()
-        queue = deque([self._s])
-        visited.add(self._s)
-
-        while queue:
-            u = queue.popleft()
-            for v, capacity in self._graph[u].items():
-                if capacity > 0 and v not in visited:
-                    queue.append(v)
-                    visited.add(v)
-
-        st_cut_flow = 0
-        for u in visited:
-            for v, capacity in self._graph[u].items():
-                if v not in visited and capacity == 0:
-                    st_cut_flow += self._graph[v][u]
-
-        return st_cut_flow
+    def ford_fulkerson(self):
+        parent = [-1] * len(self.graph)  # initialize the parent list with -1
+        max_flow = 0  # initialize the maximum flow as 0
+        while self.bfs(parent):
+            path_flow = float("Inf")  # initialize the path flow as infinity
+            s = self.t
+            # find the minimum residual capacity along the path from sink to source
+            while s != self.s:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+            max_flow += path_flow  # add the path flow to the maximum flow
+            v = self.t
+            # update the residual capacities of the edges and their reverse edges along the path
+            while v != self.s:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+        return max_flow  # return the maximum flow
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Ford-Fulkerson algorithm')
-    parser.add_argument('filename', type=str, help='input file')
-    args = parser.parse_args()
+if __name__ == '__main__':
+    try:
+        solver = MaxFlowSolver(sys.argv[1])
+        solver.solve()
+    except IndexError:
+        print("Usage: python chemin_augmentant.py <filename>")
 
-    ford_fulkerson = FordFulkerson(args.filename)
-    maxflow = ford_fulkerson.get_max_flow()
-    ford_fulkerson.save_result()
-    print(f"Le flot maximal est : {maxflow}")
+    folder = "Instances/"
+    max_flow_list = []
+    filenames = []
+    res = {}
 
-    st_cut_flow = ford_fulkerson.calculate_max_flow_st_cut()
-    print(f"Le flot maximum traversant la st-coupe est : {st_cut_flow}")
+    # Get a sorted list of filenames in the folder based on file size
+    filenamesToSort = sorted(os.listdir(folder), key=lambda x: os.path.getsize(os.path.join(folder, x)))
+    compt = 0
 
-    print("le flot maximal est-il optimal ?" + str(maxflow == st_cut_flow))
+    # Browse all instances in the folder in the order of file size
+    for filename in filenamesToSort:
+        if filename.endswith(".txt"):
+            print("filename : " + filename)
+            filename = folder + "/" + filename
+            filenames.append(filename)
+            time_start = time.time()
+            solver = MaxFlowSolver(filename)
+            maxflow = solver.solve()
+            time_end = time.time()
+            filenames.append(filename)
+
+            res[filename] = (maxflow, time_end - time_start)
+            print("count : " + str(compt))
+
+            compt += 1
+
+    print("Liste des couples (nom de fichier, flot maximal) : " + str(res))
+
+    # write the results in a txt file
+    with open("results.txt", 'w') as f:
+        for file in res:
+            f.write("filename : " + file + "\n")
+            f.write("max flow : " + str(res[file][0]) + "\n")
+            f.write("time : " + str(res[file][1]) + "\n")
+            f.write("\n")
 
